623e358c678746f80240e91a17022b0536b556ae
WarframeBuilder <warframe.builder@example.com>
2026-03-14 13:01:36 +0100

    auto 13:01:36 (1 fichiers)

--- frontend/src/components/builder/BuilderLayout.tsx
+++ frontend/src/components/builder/BuilderLayout.tsx
@@ -2,8 +2,8 @@
 
 import {
   Panel,
-  PanelGroup,
-  PanelResizeHandle,
+  Group,
+  Separator,
 } from "react-resizable-panels";
 import { Navbar } from "@/components/shared/Navbar";
 import { LeftPanel } from "@/components/builder/LeftPanel";
@@ -15,20 +15,20 @@ export function BuilderLayout() {
       <Navbar />
       <div className="flex-1 min-h-0 flex flex-col">
         <main className="h-[calc(100vh-50px)] overflow-hidden">
-          <PanelGroup direction="horizontal" className="flex h-full w-full flex-1">
+          <Group direction="horizontal" className="flex h-full w-full flex-1">
             {/* --- Panneau gauche: Stats (30%) --- */}
             <Panel defaultSize={30} minSize={20} maxSize={45}>
               <LeftPanel />
             </Panel>
 
             {/* --- Separateur draggable --- */}
-            <PanelResizeHandle className="relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 hover:bg-cyan-wakfuli/30 transition-colors" />
+            <Separator className="relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 hover:bg-cyan-wakfuli/30 transition-colors" />
 
             {/* --- Panneau droit: Equipement (70%) --- */}
             <Panel defaultSize={70}>
               <RightPanel />
             </Panel>
-          </PanelGroup>
+          </Group>
         </main>
       </div>
     </div>
